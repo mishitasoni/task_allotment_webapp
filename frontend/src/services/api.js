da1f8023b9053fb3adb50c12612914d222b2d1a4
Mishita Soni <148615731+mishitasoni@users.noreply.github.com>
2026-06-18 09:58:56 +0530

    Update API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+  baseURL:"https://task-allotment-webapp.onrender.com",
 });
 
 export default API;
